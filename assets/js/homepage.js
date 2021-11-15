fcd46d9ad5cadc6055ca98090c8efdf78cdf62af
Justin B <jbedford94@gmail.com>
2021-11-14 23:45:51 -0500

    set up boots

---
+++ assets/js/homepage.js
@@ -0,0 +1,10 @@
+const APIKey = "241dddf981d68f0a9a128039b45f252d";
+
+var cityName = document.getElementById("location")
+
+
+var getCurrentDate = function(){
+    var currentDay = moment().format("dddd, MMMM Do YYYY");
+    return currentDay;
+
+};
